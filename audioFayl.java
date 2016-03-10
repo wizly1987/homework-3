74b57abb921249066c7fbeae7750ad80f6733e09
wizly1987 <vasya.87@mail.ru>
2016-03-10 14:11:13 +0200

    Added files via upload

---
+++ audioFayl.java
@@ -0,0 +1,7 @@
+package com.goit.gojavaonline;
+
+/**
+ * Created by vasyambr on 10.03.2016.
+ */
+public class audioFayl {
+}
---
+++ deriktoriya.java
@@ -0,0 +1,12 @@
+package com.goit.gojavaonline;
+
+import java.util.List;
+
+/**
+ * Created by vasyambr on 10.03.2016.
+ */
+public class deriktoriya {
+    List<textFayl> textFayls;
+    List<audioFayl> audioFayls;
+    List<pictureFayl> pictureFayls;
+}
